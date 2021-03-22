38c7be53253b6ddff5f0336ecfdcfb1b9edf5d0c
eho99 <ca.ericho@gmail.com>
2021-03-22 09:17:00 -0700

    formatted camera_code

--- Multi Frame/camera_code.cpp
+++ Multi Frame/camera_code.cpp
@@ -9,30 +9,33 @@
 #include <typeinfo>
 #include <ctime>
 
-int main(void) {
-using namespace cv;
-xiAPIplusCameraOcv cam;
-cam.OpenFirst();
-int exposure=100000;//in us
-cam.SetExposureTime(exposure);
-cam.SetGain(15);
-cam.StartAcquisition();
-time_t timee;
+int main(void)
+{
+	using namespace cv;
+	xiAPIplusCameraOcv cam;
+	cam.OpenFirst();
+	int exposure = 100000; //in us
+	cam.SetExposureTime(exposure);
+	cam.SetGain(15);
+	cam.StartAcquisition();
+	time_t timee;
 
-while(1==1) {
-//Read and convert a frame from the camera
-Mat cv_mat_image = cam.GetNextImageOcvMat ();
-//Show image on display
-cv::imshow("Image from camera",cv_mat_image);
-timee = time(0);
-char* dt=ctime(&timee);
-//char* exp = std::to_string(exposure);
+	while (1 == 1)
+	{
+		//Read and convert a frame from the camera
+		Mat cv_mat_image = cam.GetNextImageOcvMat();
+		//Show image on display
+		cv::imshow("Image from camera", cv_mat_image);
+		timee = time(0);
+		char *dt = ctime(&timee);
+		//char* exp = std::to_string(exposure);
 
-if (cv::waitKey(10)==32){
-	cv::imwrite(std::string(dt)+"exposure"+"100000"+".jpg", cv_mat_image);
-}
-}
+		if (cv::waitKey(10) == 32)
+		{
+			cv::imwrite(std::string(dt) + "exposure" + "100000" + ".jpg", cv_mat_image);
+		}
+	}
 
-cam.StopAcquisition();
-cam.Close();
+	cam.StopAcquisition();
+	cam.Close();
 }
