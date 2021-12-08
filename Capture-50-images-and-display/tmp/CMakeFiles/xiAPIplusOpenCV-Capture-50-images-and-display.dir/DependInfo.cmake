4c1d58a52bf0ddfb4d840021cf56face0ac2fb52
Wen <chelseaw3340@tustin.k12.ca.us>
2021-12-07 17:29:05 -0800

    december 7th, 2021, trying to git push from school surface pro -- chelsea

---
+++ Capture-50-images-and-display/tmp/CMakeFiles/xiAPIplusOpenCV-Capture-50-images-and-display.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/avionics/2020-MU181/package/samples/_libs/xiAPIplusOpenCV/xiApiPlusOcv.cpp" "/home/avionics/2020-MU181/package/samples/xiAPIplusOpenCV/Capture-50-images-and-display/tmp/CMakeFiles/xiAPIplusOpenCV-Capture-50-images-and-display.dir/home/avionics/2020-MU181/package/samples/_libs/xiAPIplusOpenCV/xiApiPlusOcv.cpp.o"
+  "/home/avionics/2020-MU181/package/samples/xiAPIplusOpenCV/Capture-50-images-and-display/main.cpp" "/home/avionics/2020-MU181/package/samples/xiAPIplusOpenCV/Capture-50-images-and-display/tmp/CMakeFiles/xiAPIplusOpenCV-Capture-50-images-and-display.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../../_libs"
+  "../../../_libs/xiAPI"
+  "../../../_libs/xiAPIplusOpenCV"
+  "../$(OPENCV_DIR)/../../include"
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Capture-50-images-and-display/tmp/CMakeFiles/xiApiPlusOcv.hpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/xiApiPlusOcv.hpp.dir/xiApiPlusOcv.cpp.o"
+  "libxiApiPlusOcv.hpp.pdb"
+  "libxiApiPlusOcv.hpp.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/xiApiPlusOcv.hpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
